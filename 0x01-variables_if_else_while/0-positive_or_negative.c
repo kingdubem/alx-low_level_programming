83009e7dbe5222900050cd52a939c8efd9f9a9cb
kingdubem <chidubemgoodness14@gmail.com>
2022-09-12 08:22:33 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success/correct)
+ * main - assign a random number to the variable n each time
+ * if the number is greater than 0; is positive, *
+ * if the number is 0: is zero
+ * if the number is less than 0: is negative, followed by a newlie
+ * Return: 0
  */
+
 int main(void)
 {
 int n;
