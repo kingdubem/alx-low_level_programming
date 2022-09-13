e47797f92484d3b7daa2e7216b452aecccfb7cc8
kingdubem <chidubemgoodness14@gmail.com>
2022-09-12 20:57:57 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success/correct)
- *     */
+ * main - Entry point
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
 {
-		char i;
-
-			for (i = 'a'; i <= 'z'; i++)
-					{
-								putchar(i);
-									}
-				putchar('\n');
-					return (0);
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
