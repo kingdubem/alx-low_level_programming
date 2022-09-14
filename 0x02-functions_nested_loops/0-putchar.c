6a1b5001ea08df042e24a1a9e63eed52a32ba3bd
kingdubem <chidubemgoodness14@gmail.com>
2022-09-14 02:19:47 -0700

    today

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main programs to print putchar
+ * Return:returns 0
+ */
+
+int main(void)
+{
+int i = 0;
+char out[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+while (i < 8)
+{
+putchar(out[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
