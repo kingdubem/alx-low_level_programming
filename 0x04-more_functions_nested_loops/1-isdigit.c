29ecbac31b02cfae43447abe3bf9a059754ba880
kingdubem <chidubemgoodness14@gmail.com>
2022-09-16 21:50:00 -0700

    m

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,23 +2,14 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9)
-
- *   * @c: digit to be checked
- *
- *    * Return: 1 if c is a digit, otherwise 0
- *
- *     */
+ * @c: digit to be checked
+ * Return: 1 if c is a digit, otherwise 0
+ */
 
 int _isdigit(int c)
-
 {
-
-		if ((c >= 48) && (c <= 57))
-
-					return (1);
-
-			else
-
-						return (0);
-
+if ((c >= 48) && (c <= 57))
+return (1);
+else
+return (0);
 }
